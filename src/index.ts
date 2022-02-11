a2fcb64f326cb6a57a8afb02a33bf67ff26e1b0a
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-02-11 09:27:58 +0100

    Work in progress

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,14 @@ app.set("view engine", "njk");
 
 const formParser = express.urlencoded({ extended: true });
 
-app.post("/resources", formParser, (request, response) => {
-  response.send(JSON.stringify(request.body));
-  // request.body contains an object with our named fields
-});
+// app.post("/resources", formParser, (request, response) => {
+//   response.send(JSON.stringify(request.body));
+//   // request.body contains an object with our named fields
+// });
 
-app.get("/add-cookie", (request, response) => {
-  const favoriteTheme = post("/resources");
-    // request.body contains an object with our named fields
-  });
+app.post("/add-cookie", formParser, (request, response) => {
+  const favoriteTheme = request.body.appearance;
+  console.log(favoriteTheme);
 
   response.set(
     "Set-Cookie",
@@ -29,8 +28,7 @@ app.get("/add-cookie", (request, response) => {
       maxAge: 3600, // This is the time (in seconds) that this cookie will be stored
     }),
   );
-  console.log(favoriteTheme);
-  response.send("The cookie has been set");
+  response.redirect("/options");
 });
 
 app.use(express.static("public"));
